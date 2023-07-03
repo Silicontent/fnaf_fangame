9b77c79944c6dec1e49922306dbed0cf9dbe84ec
Silicontent <61814046+Silicontent@users.noreply.github.com>
2023-07-03 17:16:04 -0500

    Added placeholder doors

---
+++ scripts/gameplay/managers/door_manager.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_left_btn_toggled(button_pressed):
+	pass # Replace with function body.
+
+
+func _on_right_btn_toggled(button_pressed):
+	pass # Replace with function body.
